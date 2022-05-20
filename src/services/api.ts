8daf825f2a5b270931ce653e36c977f7da6dafab
charilaouc <charilaou.c@gmail.com>
2022-05-20 11:48:53 +0300

    import directus

---
+++ src/services/api.ts
@@ -0,0 +1,3 @@
+import {Directus} from "@directus/sdk"
+
+//export const directus = new Directus(process.env.REST_GRAPHQL_API_URL);
--- src/utils/get-asset-url.tsx
+++
@@ -1,4 +0,0 @@
-export function getAssetURL(id){
-    if(!id) return null;
-    return `${process.env.REST_GRAPHQL_API_URL}/assets/${id}`;
-}
